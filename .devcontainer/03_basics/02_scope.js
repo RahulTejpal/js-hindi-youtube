1e9bb84021efe4f387afa22a2705daf138e5026d
RahulTejpal <110191201+RahulTejpal@users.noreply.github.com>
2024-09-04 15:14:44 +0000

    scope 01

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,6 @@
+
+if(true){
+    var a = 200; //********we don't use 'var' because this variable "a" can also be accessed outside this brackets which is wrong
+}                // use  "let" instead of "var"   
+
+console.log(a);
