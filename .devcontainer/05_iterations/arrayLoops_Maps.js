f4d694cc7110590fd30c691b9c0e83083e522bd0
RahulTejpal <110191201+RahulTejpal@users.noreply.github.com>
2024-09-08 10:39:51 +0000

    Array high order loops and maps

---
+++ .devcontainer/05_iterations/arrayLoops_Maps.js
@@ -0,0 +1,23 @@
+//loops for arrays
+
+//******for-of loop */
+
+const greetings = "Hello world"
+for (const greet of greetings) {
+    console.log(`Each char is: ${greet}`);
+    
+}
+
+//Maps
+
+const map = new Map()
+map.set('IN','INDIA')
+map.set('USA','United States of America')
+map.set('Fr','France')
+
+// console.log(map);
+
+for (const [mapKey,mapValue] of map) {
+    console.log(mapKey,':-',mapValue);
+    
+}
